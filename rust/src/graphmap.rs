9bafa34b80084af7c91e630f43956308fecd746d
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-09-25 11:55:39 +0100

    feat: continue graphmap implementation

--- rust/src/graphmap.rs
+++ rust/src/graphmap.rs
@@ -1,18 +1,82 @@
 //! TODO
 
-use std::{collections::HashMap, marker::PhantomData};
+use std::{hash::Hash, marker::PhantomData};
+
+use indexmap::IndexMap;
 
 pub type Directed = ();
 pub type Undirected = ();
 
 #[derive(Clone, Debug)]
-pub struct GraphMap<N, E, Ty = Undirected> {
-    nodes: HashMap<N, Vec<(N, CompactDirection)>>,
-    edges: HashMap<(N, N), E>,
+pub struct GraphMap<TNode, TEdge, Ty = Undirected> {
+    nodes: IndexMap<TNode, Vec<(TNode, CompactDirection)>>,
+    edges: IndexMap<(TNode, TNode), TEdge>,
     ty: PhantomData<Ty>,
 }
 
-#[derive(Clone, Debug)]
+impl<TNode, TEdge, Ty> GraphMap<TNode, TEdge, Ty>
+where
+    TNode: Eq + Hash + Copy + Ord,
+{
+    /// Creates a new [`GraphMap<TNode, TEdge, Ty>`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Creates a new [`GraphMap<TNode, TEdge, Ty>`] with the specified capacity.
+    pub fn with_capacity(nodes: usize, edges: usize) -> Self {
+        GraphMap {
+            nodes: IndexMap::with_capacity(nodes),
+            edges: IndexMap::with_capacity(edges),
+            ty: PhantomData,
+        }
+    }
+
+    /// Returns the capacity of the graph.
+    pub fn capacity(&self) -> (usize, usize) {
+        (self.nodes.capacity(), self.edges.capacity())
+    }
+
+    /// Adds a node to the graph.
+    pub fn add_node(&mut self, node: TNode) -> TNode {
+        self.nodes.entry(node).or_insert(Vec::new());
+        node
+    }
+
+    /// Removes a node from the graph.
+    pub fn remove_node(&mut self, node: TNode) -> bool {
+        let links = match self.nodes.swap_remove(&node) {
+            Some(links) => links,
+            None => return false,
+        };
+        for (succ, dir) in links {
+            let edge = if dir == CompactDirection::Outgoing {
+                todo!("remove edge");
+                // (node, succ)
+            } else {
+                todo!("remove edge");
+                // (succ, node)
+            };
+        }
+        true
+    }
+}
+
+impl<TNode, TEdge, Ty> Default for GraphMap<TNode, TEdge, Ty>
+where
+    TNode: Eq + Hash + Copy + Ord,
+{
+    /// Creates an empty [`GraphMap<TNode, TEdge, Ty>`].
+    fn default() -> Self {
+        Self {
+            nodes: IndexMap::new(),
+            edges: IndexMap::new(),
+            ty: PhantomData::default(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
 enum CompactDirection {
     Outgoing,
     Incoming,
@@ -26,31 +90,5 @@ impl CompactDirection {
             CompactDirection::Outgoing => CompactDirection::Incoming,
             CompactDirection::Incoming => CompactDirection::Outgoing,
         }
-    }
-}
-
-impl<N, E, Ty> GraphMap<N, E, Ty>
-where
-    N: Eq + std::hash::Hash + Copy + Ord,
-{
-    pub fn new() -> Self {
-        todo!();
-    }
-
-    pub fn with_capacity(nodes: usize, edges: usize) -> Self {
-        GraphMap {
-            nodes: HashMap::with_capacity(nodes),
-            edges: HashMap::with_capacity(edges),
-            ty: PhantomData,
-        }
-    }
-
-    pub fn capacity(&self) -> (usize, usize) {
-        (self.nodes.capacity(), self.edges.capacity())
-    }
-
-    pub fn add_node(&mut self, node: N) -> N {
-        self.nodes.entry(node).or_insert(Vec::new());
-        node
     }
 }
