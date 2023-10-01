8b34de09e55e2242da749659fe2dd272e864eacd
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-10-01 20:16:49 +0100

    refactor(ts): move graphmap to its own file

--- typescript/src/cli.ts
+++ typescript/src/cli.ts
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
 
-class GraphMap<TNode, TEdge> {
-  private nodes = new Map<TNode, TNode[]>();
-  private edges = new Map<[TNode, TNode], TEdge>();
-
-  addNode(node: TNode) {
-    this.nodes.set(node, []);
-  }
-
-  addEdge(from: TNode, to: TNode, weight: TEdge) {
-    this.edges.set([from, to], weight);
-    this.nodes.get(from)?.push(to);
-  }
-}
+import { GraphMap } from "./graphmap";
 
 function main() {
+  const graph = new GraphMap<number, number>();
   console.log("Hello world!");
 }
 
---
+++ typescript/src/graphmap.ts
@@ -0,0 +1,13 @@
+export class GraphMap<TNode, TEdge> {
+  private nodes = new Map<TNode, TNode[]>();
+  private edges = new Map<[TNode, TNode], TEdge>();
+
+  addNode(node: TNode) {
+    this.nodes.set(node, []);
+  }
+
+  addEdge(from: TNode, to: TNode, weight: TEdge) {
+    this.edges.set([from, to], weight);
+    this.nodes.get(from)?.push(to);
+  }
+}
